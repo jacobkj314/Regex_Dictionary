911a4242eeba4f3864b9b4decae5d07ba9cf1489
Jacob Johnson <jacobkj314@gmail.com>
2024-05-14 09:49:09 -0600

    added support to use other languages too

--- regexd.py
+++ regexd.py
@@ -1,6 +1,10 @@
+from sys import argv
+
+language = argv[1] if len(argv) > 1 else "Spanish"
+
 import re
 
-dictionary = open("dictionaries/Spanish.dic").readlines()
+dictionary = open(f"dictionaries/{language}.dic").readlines()
 dictionary = [word.lower().strip(' ').strip('\n') for word in dictionary]
 dictionary = [re.sub(r"/.*", "", word) for word in dictionary]
 
