e503c3fa53f3be366e1d991607aa268054fac3ff
Jacob Johnson <jacobkj314@gmail.com>
2024-05-15 09:37:52 -0600

    improvements and attribution

--- regexd.py
+++ regexd.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from sys import argv
 
 language = argv[1] if len(argv) > 1 else "Spanish"
@@ -8,8 +9,10 @@ dictionary = open(f"dictionaries/{language}.dic").readlines()
 dictionary = [word.lower().strip(' ').strip('\n') for word in dictionary]
 dictionary = [re.sub(r"/.*", "", word) for word in dictionary]
 
+print(f"Welcome to my Regex dictionary. Each line you type will be interpreted as a regular expression, and all matching words in the dictionary will be returned, separated by tabs. Your selected language is {language}.\nDictionaries sourced from https://github.com/titoBouzout/Dictionarie.\nVisit https://docs.python.org/3/library/re.html for documentation on using regular expression strings.\n")
+
 while True:
-	pattern = input()
+	pattern = input('> ')
 	for word in dictionary:
 		if re.fullmatch(pattern, word):
 			print(word, end="\t")
